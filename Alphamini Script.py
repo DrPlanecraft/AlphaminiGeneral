cbe4af06be84d0d2217b97484521e6628c512b5a
DrPlanecraft <drplanecraft@hotmail.com>
2023-05-13 16:13:49 +0800

    update

--- Alphamini Script.py
+++ Alphamini Script.py
@@ -2,45 +2,39 @@ import asyncio
 import logging
 import AlphaminiConfig
 import mini.mini_sdk as MiniSdk
-from mini.dns.dns_browser import WiFiDevice
+from mini.apis.api_expression import PlayExpression # express_name (.execute())
+from mini.apis.api_action import PlayAction # action_name (.execute())
+from mini.apis.api_sound import StartPlayTTS # text (.execute())
 
-async def get_device_by_name():
-    """根据机器人序列号后缀搜索设备
-
-    搜索指定序列号(在机器人屁股后面)的机器人, 可以只输入序列号尾部字符即可,长度任意, 建议5个字符以上可以准确匹配, 10秒超时
-
-
-    Returns:
-        WiFiDevice: 包含机器人名称,ip,port等信息
-
-    """
-    result: WiFiDevice = await MiniSdk.get_device_by_name(AlphaminiConfig.SerialNum, 10)
-    print(f"test_get_device_by_name result:{result}")
-    return result
-
-async def shutdown():
-    """断开连接并释放资源
-
-    断开当前连接的设备，并释放资源
-
-    """
+# Base Code for Programming Mode
+async def exit_programmingMode():
     await MiniSdk.quit_program()
     await MiniSdk.release()
 
-async def test_start_run_program():
-    """进入编程模式demo
-
-    使机器人进入编程模式，等待回复结果，并延时6秒，让机器人播完"进入编程模式"
-
-    Returns:
-        None:
-
-    """
+async def enter_programmingMode():
     await MiniSdk.enter_program()
 
-async def main():
-    await AlphaminiConfig.setup()
-    await get_device_by_name()
-    await shutdown()
+# Programmed Action Parts go here
+TTS_say = StartPlayTTS(text="Hello There!")
+Action_Yoga = PlayAction(action_name="024")
+Expression_Sneeze = PlayExpression(express_name="codemao9")
 
-if __name__ == "__main__": asyncio.run(main())
+###########################################################################################################################################
+
+async def main():
+    # Setup for Programming Mode.
+    await MiniSdk.connect(AlphaminiConfig.setup())
+    await enter_programmingMode()
+
+    # Perform Programmed Actions
+    await TTS_say.execute()
+    await Expression_Sneeze.execute()
+    await Action_Yoga.execute()
+
+    # Exitting Programming Mode
+    await exit_programmingMode()
+
+###########################################################################################################################################
+
+if __name__ == "__main__": 
+    asyncio.run(main())
--- AlphaminiConfig.py
+++ AlphaminiConfig.py
@@ -5,10 +5,12 @@ import logging
 import mini.mini_sdk as MiniSdk
 from mini.dns.dns_browser import WiFiDevice
 
-async def setup():
+# attribuites
+SerialNum = "100893"
+
+async def setup(robotSerialNumber=SerialNum):
     # Alphamini Initiate
     MiniSdk.set_log_level(logging.INFO)
     MiniSdk.set_robot_type(MiniSdk.RobotType.EDU)
-
-# attribuites
-SerialNum = "100893"
+    device: WiFiDevice = await MiniSdk.get_device_by_name(robotSerialNumber, 10)
+    return device
